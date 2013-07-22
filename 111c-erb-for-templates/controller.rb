42346a96024cd827d6c0eacb625e86c2acdaba9c
Bryan <bew@bryanewhite.com>
2013-07-22 10:43:21 -0600

    added variables in controller and changed erb files.

--- 111c-erb-for-templates/controller.rb
+++ 111c-erb-for-templates/controller.rb
@@ -5,6 +5,9 @@ get "/" do
 end
 
 get "/reposts/1" do
+	@heading = "Spring cleanup for object creation in Ruby"
+	@summary = "A complex object creation can clutter a controller. It’s better to move it into a dedicated method of the corresponding model."
+	@link = "http://blog.8thcolor.com/2013/07/spring-cleanup-for-object-creation-in-rails/"
   halt erb(:repost1)
 end
 
