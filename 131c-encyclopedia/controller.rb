cc5bf2e47258e0d4cb330932d6727a873fee850b
Bryan <bew@bryanewhite.com>
2013-07-26 17:41:50 -0600

    Added routes and variables in controller.rb

--- 131c-encyclopedia/controller.rb
+++ 131c-encyclopedia/controller.rb
@@ -1,3 +1,12 @@
 require '../dvc-sinatra.rb'
 
-# TODO add GET handlers below
+get "/" do
+  @entries = EncyclopediaEntry.all
+  halt erb(:index)
+end
+
+get "/entries/:name" do
+  id = params[:name]
+  @entry = EncyclopediaEntry.find(id)
+  halt erb(:show)
+end
