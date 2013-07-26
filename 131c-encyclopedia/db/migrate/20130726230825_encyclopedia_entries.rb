3a46635a43f5e4a7281d250e72a65b57b8f823fd
Bryan <bew@bryanewhite.com>
2013-07-26 17:24:42 -0600

    Fixed migration and ran db:migrate sucessfully

--- 131c-encyclopedia/db/migrate/20130726230825_encyclopedia_entries.rb
+++ 131c-encyclopedia/db/migrate/20130726230825_encyclopedia_entries.rb
@@ -1,12 +1,12 @@
 class EncyclopediaEntries < ActiveRecord::Migration
   def up
-  	create table :encyclopediaentries do |t|
-  	  t.string = :word,
-  	  t.text = :content
+  	create_table :encyclopedia_entries do |t|
+  	  t.string :word
+  	  t.text :content
   	end
   end
 
   def down
-  	drop_table :encyclopediaentries
+  	drop_table :encyclopedia_entries
   end
 end
