19a68c499d978be3b669221d3c317baa5261e51a
Bryan <bew@bryanewhite.com>
2013-07-26 19:40:27 -0600

    Added seed data.

--- 132c-world-cities/db/seeds.rb
+++ 132c-world-cities/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+City.create(city: 'London',
+			country: 'United Kingdom',
+			flag: "gb.png",
+			timezone: 0,
+			population: 8.3
+			)
+
+City.create(city: 'New York City',
+			country: 'United States of America',
+			flag: "us.png",
+			timezone: -5,
+			population: 8.3
+			)
+
+City.create(city: 'Shanghai',
+			country: 'China',
+			flag: "cn.png",
+			timezone: +8,
+			population: 23.0
+			)
