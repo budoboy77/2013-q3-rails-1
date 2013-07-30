1a8ce24dad88e0d6de741509fbaebca5a88936ad
Bryan <bew@bryanewhite.com>
2013-07-29 21:42:14 -0600

    finished mockup 2

--- 137c-only-two-records/controller.rb
+++ 137c-only-two-records/controller.rb
@@ -5,3 +5,12 @@ get "/" do
   halt erb(:root)
 end
 
+get "/line1" do
+  @bracelet_line = BraceletLine.find(1)
+  halt erb(:edit)
+end
+
+get "/line2" do
+  @bracelet_line = BraceletLine.find(2)
+  halt erb(:edit)
+end
