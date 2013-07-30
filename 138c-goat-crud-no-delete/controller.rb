256abcb55965565330ad08ba99b271b3ecfe4fec
Bryan <bew@bryanewhite.com>
2013-07-29 22:07:57 -0600

    added route and index.erb

--- 138c-goat-crud-no-delete/controller.rb
+++ 138c-goat-crud-no-delete/controller.rb
@@ -1,2 +1,16 @@
 require '../dvc-sinatra.rb'
 
+get '/' do
+	@goats = RacingGoat.all
+	halt erb(:index)
+end
+
+get '/new-goat' do
+	halt erb(:new)
+end
+
+get '/:id' do
+  id = params[:id]
+  @goat = RacingGoat.find(id)
+  halt erb(:edit)
+end
