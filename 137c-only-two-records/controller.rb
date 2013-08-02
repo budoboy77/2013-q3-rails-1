df5d2b1dfb9a14b16cf420fa1cb2991ee5d0a0dd
Bryan <bew@bryanewhite.com>
2013-08-01 20:50:38 -0600

    finished 137c

--- 137c-only-two-records/controller.rb
+++ 137c-only-two-records/controller.rb
@@ -10,7 +10,23 @@ get "/line1" do
   halt erb(:edit)
 end
 
-get "/line2" do
+post "/line1" do
+  bracelet_line  			= BraceletLine.find(1)
+  bracelet_line.font_size	= params[:font_size]
+  bracelet_line.text		= params[:text]
+  bracelet_line.save!
+  redirect "/"
+end
+
+  get "/line2" do
   @bracelet_line = BraceletLine.find(2)
   halt erb(:edit)
+end
+
+post "/line2" do
+  bracelet_line  			= BraceletLine.find(2)
+  bracelet_line.font_size	= params[:font_size]
+  bracelet_line.text		= params[:text]
+  bracelet_line.save!
+  redirect "/"
 end
