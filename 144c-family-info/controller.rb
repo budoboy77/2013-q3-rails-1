e4c815b29e7c0df97dd0ad40be399a0ab1003427
Bryan <bew@bryanewhite.com>
2013-08-03 11:14:01 -0600

    Added post handle for family information form

--- 144c-family-info/controller.rb
+++ 144c-family-info/controller.rb
@@ -6,5 +6,14 @@ get "/" do
 end
 
 post "/" do
+	person 						= Person.new
+	person.relation_to_filer 	= params[:relation_to_filer]
+	person.first_name			= params[:first_name]
+	person.last_name			= params[:last_name]
+	person.date_of_birth		= params[:date_of_birth]
+	person.gender				= params[:gender]
+	person.is_dependent			= params[:is_dependent]
+	person.save!
+	redirect "/"
   # TODO: write this part
 end
