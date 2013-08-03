b5eab24fba67d410792d56d56ef1cb3fddd80867
Bryan <bew@bryanewhite.com>
2013-08-03 11:38:23 -0600

    finished 139c

--- 139c-hardcoded-login/controller.rb
+++ 139c-hardcoded-login/controller.rb
@@ -1,11 +1,22 @@
 require '../dvc-sinatra.rb'
 
 get "/" do
+  
   halt erb(:login)
 end
 
 post "/" do
-  # TODO: write this part
+  if params[:username] != "amy"
+  	@error = "Login failed!!!!"
+  	@old_username = params[:username]
+  	halt erb(:login)
+  elsif params[:password] != "123"
+  	@error = "Password incorrect!!!"
+  	@old_username = params[:username]
+    halt erb(:login)
+  else
+  	redirect "/welcome"
+  end
 end
 
 get "/welcome" do
