097de37f577b0beb3c35fcc58f1f5426c04e53ce
Bryan <bew@bryanewhite.com>
2013-08-08 08:17:56 -0600

    Completed 152c

--- 152c-iphones-and-get-params/controller.rb
+++ 152c-iphones-and-get-params/controller.rb
@@ -8,8 +8,28 @@ get "/listings" do
   model = params[:model]
   gigs = params[:gigs]
 
-  # TODO: Assign to @listings variable, based on values of model and gigs
-  #       variables.  You'll need to handle nil and non-nil values separately
+	if gigs != nil
+		if model != nil
+  			@listings = Listing.where(gigs: gigs).where(model: model).order("price desc")
+  		else
+  			@listings =  Listing.where(gigs: gigs).order("price desc")
+  		end
+  	elsif
+  		if model != nil
+  			@listings = Listing.where(model: model).order("price desc")
+  		end
+  	else
+  		@listings =  Listing.order("price desc").all
+  	end
+  # else
+  #	@listings = Listing.all
+ # end
+
+  # if model != nil
+  # 	@listings =  Listing.where(model: model)
+  # else
+  # 	@listings = Listing.all
+  # end
 
   halt erb(:listings)
 end
