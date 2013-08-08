7f5c3a3cfedd7130f038840c54a903b5275a8d4b
Bryan <bew@bryanewhite.com>
2013-08-07 21:21:57 -0600

    Finished 149c.

--- 149c-elements/controller.rb
+++ 149c-elements/controller.rb
@@ -1,14 +1,14 @@
 require '../dvc-sinatra.rb'
 
 get "/" do
-  halt erb(:questions)
+	halt erb(:questions)
 end
 
 post "/" do
   if params[:name_of_element] != ""
-    @answer9 = "TODO: write the answer here"
+    @answer9 = Element.where("name ilike ?", params[:name_of_element]).first.number
   elsif params[:number_of_element] != ""
-    @answer10 = "TODO: write the answer here"
+    @answer10 = Element.where(number: params[:number_of_element]).first.name
   end
   halt erb(:answer_to_9_or_10)
 end
