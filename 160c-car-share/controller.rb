3de80e3d652b28a984de13c916592d2c5cde5622
Bryan <bew@bryanewhite.com>
2013-08-12 21:11:43 -0600

    Worked on 160c.  Completed through reservations.

--- 160c-car-share/controller.rb
+++ 160c-car-share/controller.rb
@@ -5,9 +5,32 @@ get "/" do
 end
 
 get "/login" do 
+  @username = params[:username]
   halt erb(:login)
 end
 
-get "/reservations/1" do
+post "/login" do
+  @username = params[:username]
+
+  [1, 2].each do |member_id|
+    member = Member.find(member_id)
+    if @username == member.username
+      if params[:password] == member.password
+        redirect "/reservations/#{member_id}"
+      else
+        @error = "Incorrect Password"
+        halt erb(:login)
+      end
+    end
+  end
+  
+  @error = "Unknown Username"
+  halt erb(:login)
+
+end
+
+get "/reservations/:id" do
+  member = Member.find(params[:id])
+  @name = member.username
   halt erb(:reservations)
 end
