d3c6f214251179c37500b4ad91db5aefa7a2fd59
Bryan <bew@bryanewhite.com>
2013-08-22 09:21:54 -0600

    Completed 173c

--- 173c-friend-book/controller.rb
+++ 173c-friend-book/controller.rb
@@ -12,6 +12,7 @@ end
 post "/login" do
 	@people = Person.order(:name).all
   if Person.where(name: params[:username]).first.authenticate(params[:password]) != false
+    session[:user_id] = Person.where(name: params[:username]).first.id
   	redirect "/followers"
   else
   	halt erb(:login)
@@ -19,11 +20,21 @@ post "/login" do
 end
 
 get "/followers" do
+  @people = Person.order(:name).all
   halt erb(:followers)
 end
 
 post "/followers" do
-  # TODO: write this
+  @people = Person.order(:name).all
+  if params[:username] != ""
+    follow =  Following.new
+    follow.to_person_id = Person.where(name: params[:username]).first.id
+    follow.from_person_id = session[:user_id]
+    follow.save!
+    redirect "/followers"
+  else
+    halt erb(:followers)
+  end
 end
 
 get "/logout" do
