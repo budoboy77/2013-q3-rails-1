4fa93e61fc8de98d7958cc1aa726f95abfa5bcb0
Bryan <bew@bryanewhite.com>
2013-08-23 12:22:12 -0600

    Class work

--- 173c-friend-book/models/person.rb
+++ 173c-friend-book/models/person.rb
@@ -1,5 +1,7 @@
 class Person < ActiveRecord::Base
+	validates :name, uniqueness: true
 	has_many :from_person, class_name: "Following", foreign_key: "from_person_id"
 	has_many :to_person, class_name: "Following", foreign_key: "to_person_id"
 	has_secure_password
+
 end
